5e0673195bfb42709f1bc1cc9ebe847cc97d6d58
Avinash <avinashdeshan11@gmail.com>
2025-04-29 11:31:56 +0530

    add method to repository

--- src/main/java/edu/icet/repository/EmployeeRepository.java
+++ src/main/java/edu/icet/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package edu.icet.repository;
 
+import edu.icet.dto.Employee;
 import edu.icet.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+    Optional<Employee> findByEmail(String email);
 }
